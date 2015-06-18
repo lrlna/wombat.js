76ffd4f326432618a7bf1b087d4f7f11384222b9
irina <shestak.irina@gmail.com>
2015-06-18 11:36:27 -0700

    adding some colour

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 #! /usr/bin/env node
-var path = require("path");
-var fs = require("fs");
+var path = require("path")
+var fs = require("fs")
+var clc = require("cli-color")
 var argv = require("yargs")
 .usage("Usage: [$0] your text here.")
-.argv;
+.describe({
+  "b": "blue wombat",
+  "y": "yellow wombat",
+  "g": "green wombat",
+  "r": "red wombat"
+})
+.boolean(["b", "y", "g", "r"])
+.argv
 
 if (argv._.length){
-  displayWombat(argv._);
+  displayWombat(argv)
 } else if (argv["$0"]) {
-  require("yargs").showHelp();
+  require("yargs").showHelp()
 }
 
-function displayWombat (thoughts) {
-  var filepath = path.join(__dirname, "wombat.butts");
-  var wombat = fs.readFileSync(filepath, "utf-8");
-  console.log(wombat);
-  console.log(concatArgvs(thoughts));
+function displayWombat (argv) {
+  var filepath = path.join(__dirname, "wombat.butts")
+  var wombat = fs.readFileSync(filepath, "utf-8")
+  console.log(colourTheWombat(wombat, argv))
+};
+
+function colourTheWombat (wombat, argv) {
+  if (argv.b) {
+    return clc.blue(wombat + concatArgvs(argv._))
+  } else if (argv.y) {
+    return clc.yellow(wombat + concatArgvs(argv._))
+  } else if (argv.g) {
+    return clc.green(wombat + concatArgvs(argv._))  
+  } else if (argv.r) {
+    return clc.red(wombat + concatArgvs(argv._)) 
+  } else {
+    return wombat + concatArgvs(argv._) 
+  }
 };
 
 function concatArgvs (thoughts) {
-  var wombatThoughts = thoughts.join(" ");
+  var wombatThoughts = thoughts.join(" ")
   var lines = {
     open: "<----(",
     close: ")---->"
   }
-  return putThoughtTogether(lines, wombatThoughts);
+  return putThoughtTogether(lines, wombatThoughts)
 };
 
 function putThoughtTogether (lines, wombatThoughts) {
-  return lines.open + wombatThoughts + lines.close;
+  return lines.open + wombatThoughts + lines.close
 };
