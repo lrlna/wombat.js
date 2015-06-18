62a8871edae2e2f93acbcdc93ec9365de5b72389
irina <shestak.irina@gmail.com>
2015-06-18 09:13:33 -0700

    basic say bubble, removed options

--- index.js
+++ index.js
@@ -2,40 +2,31 @@
 var path = require("path");
 var fs = require("fs");
 var argv = require("yargs")
-.usage("[$0] [-o] text.\n")
-.command({
-  "h": "help is on the way",
-  "i": "do something else"
-})
-.boolean(["h", "i"])
+.usage("Usage: [$0] your text here.")
 .argv;
 
-if (argv.h) {
-  require("yargs").showHelp();
-} else if (argv._.length){
+if (argv._.length){
   displayWombat(argv._);
-} else if (!argv) {
+} else if (argv["$0"]) {
   require("yargs").showHelp();
 }
 
 function displayWombat (thoughts) {
   var filepath = path.join(__dirname, "wombat.butts");
   var wombat = fs.readFileSync(filepath, "utf-8");
-  console.log(wombat + "\n");
+  console.log(wombat);
   console.log(concatArgvs(thoughts));
 };
 
 function concatArgvs (thoughts) {
   var wombatThoughts = thoughts.join(" ");
   var lines = {
-    top: "/",
-    middle: "/", 
     open: "<----(",
     close: ")---->"
   }
-  putThoughtTogether(lines, wombatThoughts);
+  return putThoughtTogether(lines, wombatThoughts);
 };
 
 function putThoughtTogether (lines, wombatThoughts) {
-  return lines.top + "\n" + lines.middle + "\n" + lines.open + wombatThoughts + close;
+  return lines.open + wombatThoughts + lines.close;
 };
